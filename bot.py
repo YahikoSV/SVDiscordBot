7996df551eded81e24c26216b9a2f0acaf75b361
YahikoDaichi <yahikodaichi@gmail.com>
2020-12-10 23:10:02 +0900

    Added deckcode command

--- bot.py
+++ bot.py
@@ -19,6 +19,8 @@ nest_asyncio.apply()
 
 from flask import Flask
 from threading import Thread
+import urllib.request
+import json
 
 app = Flask('')
 
@@ -37,78 +39,13 @@ def keep_alive():
     server.start()
 
 
-# load_dotenv()
-# TOKEN = os.getenv('DISCORD_TOKEN')
-
-# client = discord.Client()
-
-# @client.event
-# async def on_ready():
-#     print(f'{client.user} has connected to Discord!')
-
-# client.run(TOKEN)
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
-
-#client = discord.Client()
-
-# @client.event
-# async def on_ready():
-#     guild = discord.utils.get(client.guilds, name=GUILD)
-#     print(
-#         f'{client.user} is connected to the following guild:\n'
-#         f'{guild.name}(id: {guild.id})'
-#     )
-
-# @client.event
-# async def on_ready():
-#     print(f'{client.user.name} has connected to Discord!')
-
-# @client.event
-# async def on_member_join(member):
-#     await member.create_dm()
-#     await member.dm_channel.send(
-#         f'Hi {member.name}, welcome to my Discord server!'
-#     )
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     brooklyn_99_quotes = [
-#         'I\'m a fox!',
-#         (
-#             'Cool. Cool cool cool cool cool cool cool, '
-#             'no doubt no doubt no doubt no doubt.'
-#         ),
-#     ]
-
-#     if message.content == 'Cat':
-#         response = random.choice(brooklyn_99_quotes)
-#         await message.channel.send(response)
-
-# @client.event
-# async def on_error(event, *args, **kwargs):
-#     with open('err.log', 'a') as f:
-#         if event == 'on_message':
-#             f.write(f'Unhandled message: {args[0]}\n')
-#         else:
-#             raise
-
-# client.run(TOKEN)
-
-# 1
-
-# 2
-bot = commands.Bot(command_prefix='!faq ', case_insensitive=True)
-status = cycle(['with Python', 'JetHub'])
+bot = commands.Bot(command_prefix='!faq ', case_insensitive=True, intents=intents)
+status = cycle(['with Python', 'MikoHub'])
 
 
 @bot.event
@@ -121,39 +58,46 @@ async def change_status():
     await bot.change_presence(activity=discord.Game(next(status)))
 
 
-@bot.command(name='Fubuki_is_Cat', help='Responds with a random quote from Fubuki')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'No not cat! I\'m Fox', 'Kitsune jyaa!'
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-
-
-@bot.command(name='roll', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
-
-
-@bot.command(name='create-channel')
-@commands.has_role('admin')
-async def create_channel(ctx, channel_name):
-    guild = ctx.guild
-    existing_channel = discord.utils.get(guild.channels, name=channel_name)
-    if not existing_channel:
-        print(f'Creating a new channel: {channel_name}')
-        await guild.create_text_channel(channel_name)
-
 
 @bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the Discord server FAQ!'
+    )
+
+@bot.command(name='Fubuki_is_Cat', help='Responds with a random quote from Fubuki')
+async def Fubuki_Cat(ctx):
+    Fubuki_quotes = [
+        'No not cat! I\'m Fox', 'Kitsune jyaa!', 'Nyaa~'
+    ]
+    response = random.choice(Fubuki_quotes)
+    await ctx.send(response)
+    
+@bot.command(name='deckcode', help='Returns SV Portal link from deckcode')
+async def svdeckcode(ctx, code, mode='R', lang='en'):
+    deck_code = code
+    deck_code_url = "https://shadowverse-portal.com/api/v1/deck/import?format=json&deck_code=" + deck_code + "&lang=en"
+    sv_format = {'R':'3', 'U':'1'}
+    languages = ['en', 'ja', 'ko', 'zh-tw' , 'fr', 'it', 'de', 'es']
+    
+    with urllib.request.urlopen(deck_code_url) as response:
+            source = response.read()
+            
+    deck_json = json.loads(source)
+    if len(deck_json['data']['errors']) != 0:
+        response = "Deck code invalid or does not exist"
+    elif mode.upper() not in sv_format:
+        response = "Invalid deck format"
+    elif lang.lower() not in languages:
+        response = "Invalid language"
+    else:      
+        deck_hash = deck_json['data']['hash']
+        deck_hash = deck_hash.replace("1",str(sv_format[mode.upper()]),1)
+        
+        deck_list_url = "https://shadowverse-portal.com/deck/" + str(deck_hash) + "?lang=" + str(lang)
+        response = deck_list_url
+    await ctx.send(response)
 
 
 bot.run(TOKEN)
