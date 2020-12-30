9d84f1dc900a9b610aad65a0824be4e79cee6219
YahikoDaichi <yahikodaichi@gmail.com>
2020-12-30 11:20:09 +0900

    Error Catching for mull

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 import requests
 import random as rand
 from bs4 import BeautifulSoup as bs
-from itertools import permutations 
+from itertools import permutations
 
 import nest_asyncio
 import asyncio
@@ -88,61 +88,115 @@ async def Fubuki_Cat(ctx):
 @bot.command(name='dctdl', help='Returns SV Portal link from deckcode')
 async def svcodetostatic(ctx, deck_code, lang='en', mode='u'):
     
-    response = createlinkfromcode(deck_code, lang, mode)
+    response, valid_input = createlinkfromcode(deck_code, lang, mode)
     await ctx.send(response)
     
     
 @bot.command(name='dbtdl', help='Turns SV builder links into static links')
 async def svbuildtostatic(ctx, link, lang='en', mode='u'):
 
-    response = createlinkfrombuilder(link, lang, mode)
+    response, valid_input = createlinkfrombuilder(link, lang, mode)
     await ctx.send(response)
 
 
 @bot.command(name='mull', help='Test out a mulligan')
 async def openingmull(ctx, deck_code, lang='en', mode='u'):
     
-    link = createlinkfromcode(deck_code, lang, mode)
-    card_list = decklist(link)
-       
-    draw_count = 3
+    response, valid_input = createlinkfromcode(deck_code, lang, mode)
     
-    while True:
-        opening_hand = rand.sample(card_list,draw_count)
-        description = f' 1. {opening_hand[0]}\n 2. {opening_hand[1]}\n 3. {opening_hand[2]}'
-        response = discord.Embed(title="Opening_Hand", description = description, color=0xffb7c5)
-        response.set_footer(text='Type card nos. to mull. (Type n if keep hand): ')
-        await ctx.send(embed=response)
-        
-        def check(msg):
-            return msg.author == ctx.author and msg.channel == ctx.channel
-        try:
-            msg = await bot.wait_for("message", check=check, timeout=20)
-            opening_hand = mulliganfunction(msg.content, card_list, opening_hand)
-        except asyncio.TimeoutError:
-            await ctx.send("Time's yp! FAQ! (Mulligan Ended)")
-        
-        description = f' 1. {opening_hand[0]}\n 2. {opening_hand[1]}\n 3. {opening_hand[2]}'
-        response = discord.Embed(title="Final_Hand", description = description, color=0xffb7c5)
-        response.set_footer(text='Try Again. (Type y if yes): ')
-        await ctx.send(embed=response)
-        
-        try:
-            msg = await bot.wait_for("message", check=check, timeout=10)
-            if msg.content.lower() == "y":
-                continue
-            else:
-                await ctx.send("Mulligan Ended")
-                break
-        except asyncio.TimeoutError:
-            await ctx.send("Time's up! FAQ! (Mulligan Ended)")
-            break
+    if valid_input == False:
+            await ctx.send(response)
+    else:
+       
+        card_list = decklist(response)     
+        draw_count = 3
+        time_out = False
+        while time_out == False:
+            author = ctx.author.name
+            txt_response =  f"**{author}**, choose cards that you want to mulligan."
+            await ctx.send(txt_response)
+            opening_hand = rand.sample(card_list,draw_count)
+            description = f' Ooo {opening_hand[0]}\n \
+                             oOo {opening_hand[1]}\n \
+                             ooO {opening_hand[2]}'
+                             
+            embed_response = discord.Embed(title="Opening Hand", description = description, color=0xffb7c5)
+            embed_response.set_footer(text='x = mull, o = keep (eg. xox, oxx, ooo)')
+            await ctx.send(embed=embed_response) 
+    
+            
+            def check(msg):
+                return msg.author == ctx.author and msg.channel == ctx.channel
+            
+            valid_mull = False
+            while valid_mull == False:
+                try:
+                    msg = await bot.wait_for("message", check=check, timeout=60)
+                    opening_hand, valid_mull = mulliganfunction(ctx, msg.content, card_list, opening_hand, valid_mull)
+                except asyncio.TimeoutError:
+                    await ctx.send(f"**{author}**, Time's yp! FAQ! (Mulligan Ended)")
+                    valid_mull = True
+                    time_out = True
+                
+                if valid_mull == False:
+                    await ctx.send(f"**{author}**, Invalid Mulligan please enter a valid mulligan (eg. oxo, xoo, xxx)")
+                
+            if time_out == False:
+                description = f' {opening_hand[0]}\n \
+                                 {opening_hand[1]}\n \
+                                 {opening_hand[2]}'
+                embed_response = discord.Embed(title="Final Hand", description = description, color=0xffb7c5)
+                embed_response.set_footer(text='Try Again? (Enter y if yes): ')
+                await ctx.send(embed=embed_response)
+                
+                try:
+                    msg = await bot.wait_for("message", check=check, timeout=10)
+                    if msg.content.lower() == "y":
+                        continue
+                    else:
+                        await ctx.send(f"**{author}**, Mulligan Ended")
+                        break
+                except asyncio.TimeoutError:
+                    await ctx.send(f"**{author}**, Time's up! FAQ! (Mulligan Ended)")
+                    break
 
 
+# @bot.event
+# async def on_message(message):
+#     # No infinite bot loops
+#     if message.author == bot.user or message.author.bot:
+#         return
+
+#     if message.content == 'great':
+#         mention = message.author.mention
+#         response = f"hey {mention}, you're great!"
+#         await message.channel.send(response)
+
+# @bot.event
+# async def on_message2(message):
+#     if message.content.startswith('$greet'):
+#         channel = message.channel
+#         await channel.send('Say hello!')
+
+#         def check(m):
+#             return m.content == 'hello' and m.channel == channel
+
+#         msg = await bot.wait_for('message', check=check)
+#         await channel.send('Hello {.author}!'.format(msg))
+        
+@bot.command(name='greet')
+async def on_message3(ctx):
+    # No infinite bot loops
+    if ctx.author == bot.user or ctx.author.bot:
+        return
+
+    mention = ctx.author.name
+    response = f"hey {mention}, you're great!"
+    await ctx.channel.send(response)
 #################### Functions ######################
 
 
-def createlinkfrombuilder(deck_builder_url, lang, mode):
+def createlinkfrombuilder(deck_builder_url, lang, mode, valid_input = False):
     sv_format = {'R':'3', 'U':'1'}
     languages = ['en', 'ja', 'ko', 'zh-tw' , 'fr', 'it', 'de', 'es']            
 
@@ -158,10 +212,11 @@ def createlinkfrombuilder(deck_builder_url, lang, mode):
         deck_list_url = "https://shadowverse-portal.com/deck/" + str(deck_hash) + "?lang=" + str(lang)
         #response = discord.Embed(title="Sample Embed", url=deck_list_url)
         response = deck_list_url
-    return response
+        valid_input = True
+    return response, valid_input
 
 
-def createlinkfromcode(deck_code, lang, mode):
+def createlinkfromcode(deck_code, lang, mode, valid_input = False):
     sv_format = {'R':'3', 'U':'1'}
     languages = ['en', 'ja', 'ko', 'zh-tw' , 'fr', 'it', 'de', 'es']
     
@@ -182,7 +237,8 @@ def createlinkfromcode(deck_code, lang, mode):
         
         deck_list_url = "https://shadowverse-portal.com/deck/" + str(deck_hash) + "?lang=" + str(lang)
         response = deck_list_url
-    return response
+        valid_input = True
+    return response, valid_input 
 
 
 def mullcards(card_list, opening_hand, card_to_mull):
@@ -219,27 +275,40 @@ def decklist(link):
     return card_list
 
 
-def mulliganfunction(mulligan, card_list, opening_hand):
-    x = '123'
-    if mulligan.lower() == 'n':
-        pass
-    elif mulligan in [''.join(j) for i in range(1,len(x) + 1) for j in  permutations(x, i)]:
-        if mulligan == '1':
-            card_to_mull = [1]
-        elif mulligan == '2':
-            card_to_mull = [2]
-        elif mulligan == '3':
-            card_to_mull = [3]
-        elif mulligan in [''.join(p) for p in permutations('12')]:
-            card_to_mull = [1,2]
-        elif mulligan in [''.join(p) for p in permutations('23')]:
-            card_to_mull = [2,3]
-        elif mulligan in [''.join(p) for p in permutations('13')]:
-            card_to_mull = [1,3]
-        elif mulligan in [''.join(p) for p in permutations('123')]:
-            card_to_mull = [1,2,3]
-        opening_hand = mullcards(card_list, opening_hand, card_to_mull)
+# def mulliganfunction(mulligan, card_list, opening_hand):
+#     x = '123'
+#     if mulligan.lower() == 'n':
+#         pass
+#     elif mulligan in [''.join(j) for i in range(1,len(x) + 1) for j in  permutations(x, i)]:
+#         if mulligan == '1':
+#             card_to_mull = [1]
+#         elif mulligan == '2':
+#             card_to_mull = [2]
+#         elif mulligan == '3':
+#             card_to_mull = [3]
+#         elif mulligan in [''.join(p) for p in permutations('12')]:
+#             card_to_mull = [1,2]
+#         elif mulligan in [''.join(p) for p in permutations('23')]:
+#             card_to_mull = [2,3]
+#         elif mulligan in [''.join(p) for p in permutations('13')]:
+#             card_to_mull = [1,3]
+#         elif mulligan in [''.join(p) for p in permutations('123')]:
+#             card_to_mull = [1,2,3]
+#         opening_hand = mullcards(card_list, opening_hand, card_to_mull)
     
-    return opening_hand
+#     return opening_hand
+
+def mulliganfunction(ctx, mulligan, card_list, opening_hand, valid_mull):
+    answer = 'oooxxx'
+    if mulligan.lower() not in set([''.join(p) for p in permutations(answer,3)]):
+        pass
+    else:
+        card_to_mull = [card + 1 for card in range (0,len(mulligan)) if mulligan[card] == 'x']
+        opening_hand = mullcards(card_list, opening_hand, card_to_mull)
+        valid_mull = True
+        
+    return opening_hand, valid_mull
+
+    
     
 bot.run(TOKEN)
